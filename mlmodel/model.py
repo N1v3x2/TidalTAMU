eaafac3905d789793b4962a8c2d68681a1a7cdf5
vadit <59973483+vadit@users.noreply.github.com>
2023-11-11 13:09:25 -0600

    Update model.py

--- mlmodel/model.py
+++ mlmodel/model.py
@@ -1,12 +1,13 @@
-#important: setup venv and have pandas and scikit learn installed before running
+# important: setup venv and have pandas and scikit learn installed before running
 
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 
-#test data by init dataframe
-
-def predict_final(work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes, listen, prevgpa):
+def predict_final(work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep,
+                  notes, listen, prevgpa):
     stuperf_path = "StudentsPerformance_with_headers.csv"
     stuperf_data = pd.read_csv(stuperf_path)
     # print(stuperf_data.columns)
@@ -17,26 +18,45 @@ def predict_final(work, extracurricular, partner, studyhours, rf1, rf2, seminar,
                         'Impact of your projects/activities on your success',
                         'Attendance to classes', 'Preparation to midterm exams 2', 'Taking notes in classes',
                         'Listening in classes', 'Cumulative grade point average in the last semester (/4.00)']
-
     x = stuperf_data[stuperf_features]
-    # print(x.describe())
-    # print(x.head())
 
-    stuperf_model = DecisionTreeRegressor(random_state=1)
-    stuperf_model.fit(x, y)
-    data = {
-        'Additional work': [work], 'Regular artistic or sports activity': [extracurricular], 'Do you have a partner': [partner],
-        'Weekly study hours': [studyhours], 'Reading frequency': [rf1],
-        'Reading frequency.1': [rf2], 'Attendance to the seminars/conferences related to the department': [seminar],
-        'Impact of your projects/activities on your success': [projects],
-        'Attendance to classes': [attendance], 'Preparation to midterm exams 2': [midtermprep], 'Taking notes in classes': [notes],
-        'Listening in classes': [listen], 'Cumulative grade point average in the last semester (/4.00)': [prevgpa]
-    }
-    # print("predicting gpa for head data:")
-    # print(inpdata)
-    # print("calculated final grade is:")
-    # print(stuperf_model.predict(inpdata))
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+    scaler = StandardScaler()
+    x_train = scaler.fit_transform(x_train)
+    x_test = scaler.transform(x_test)
+
+    stuperf_model = LogisticRegression()
+    stuperf_model.fit(x_train, y_train)
+
+    # accuracy = stuperf_model.score(x_test, y_test)
+    # print("Accuracy:", accuracy)
+
+    data = [[work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            [work, extracurricular, partner, studyhours, rf1, rf2, seminar, projects, attendance, midtermprep, notes,
+             listen, prevgpa],
+            ]
     inpdata = pd.DataFrame(data=data)
-    return (stuperf_model.predict(inpdata)[0]+2)
-
-
+    return stuperf_model.predict(inpdata)[0] + 1
